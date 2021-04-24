91b8b52f58905b603271c7c55f5b2054233e7fa0
LSqw <lukasstolz99e@gmail.com>
2021-04-24 23:14:03 +0200

    Added maxProps and bottomPositon props to layouts

--- Widgets/Column.py
+++ Widgets/Column.py
@@ -3,16 +3,25 @@ from PygameWidgets.Widgets.Widget import Widget
 
 class Column(Widget):
 
-    def __init__(this, window, x, y, Widgets):
-        super().__init__(window, x, y, 0, window.get_height(), None)
+    def __init__(this, window, x, y, Widgets, maxHeight = None):
+        if maxHeight == None:
+            maxHeight = window.get_height()
+        super().__init__(window, x, y, 0, maxHeight, None)
         for widget in Widgets:
-            if widget.w > this.w:
-                this.w = widget.w
+            if widget.w + widget.x > this.w + this.x:
+                this.w = widget.w + widget.x
             widget.x += this.x
             widget.y += this.y
 
         this.Widgets = Widgets
 
     def draw(this):
+        currentHeight = 0
         for widget in this.Widgets:
-            widget.draw()
+            currentHeight += widget.h
+            if currentHeight <= this.h:
+                widget.draw()
+
+    @property
+    def bottomPosition(this):
+        return this.y + this.h
--- Widgets/Row.py
+++ Widgets/Row.py
@@ -3,16 +3,25 @@ from PygameWidgets.Widgets.Widget import Widget
 
 class Row(Widget):
 
-    def __init__(this, window, x, y, Widgets):
-        super().__init__(window, x, y, window.get_width(), 0, None)
+    def __init__(this, window, x, y, Widgets, maxWidth = None):
+        if maxWidth == None:
+            maxWidth = window.get_width()
+        super().__init__(window, x, y, maxWidth, 0, None)
         for widget in Widgets:
-            if widget.h > this.h:
-                this.h = widget.h
+            if widget.h + widget.y > this.h + this.y:
+                this.h = widget.h + widget.y
             widget.x += this.x
             widget.y += this.y
 
         this.Widgets = Widgets
 
     def draw(this):
+        currentWidth = 0
         for widget in this.Widgets:
-            widget.draw()
+            currentWidth += widget.w
+            if(currentWidth <= this.w):
+                widget.draw()
+
+    @property
+    def bottomPosition(this):
+        return this.y + this.h
