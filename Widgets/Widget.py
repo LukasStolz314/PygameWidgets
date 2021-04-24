3ea247edba487b8c6f461223598a53681cfc8660
Lukas <lukas.stolz9@outlook.de>
2021-04-24 15:37:08 +0200

    Added Widget

---
+++ Widgets/Widget.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+class Widget():
+
+    def __init__(this, window, x, y, w, h, backgroundColor):
+        this.window = window
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.backgroundColor = backgroundColor
+
+    def draw(this):
+        pass
