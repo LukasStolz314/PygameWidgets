5a7ab3a98d0f7550b9a2fec2384c97aa6e2d0c19
LSqw <lukasstolz99e@gmail.com>
2021-04-25 18:57:38 +0200

    Refactored WidgetList

--- Widgets/Page.py
+++ Widgets/Page.py
@@ -2,18 +2,18 @@ import pygame
 
 class Page:
 
-    def __init__(this, window, name):
+    def __init__(this, window, name, widgetList):
         this.window = window
         this.name = name
-        this.widgets = [] 
+        this.widgetList = widgetList
     
     def addWidget(this, widget):
-        this.widgets.append(widget)
+        this.widgetList.append(widget)
     
     def drawWidgets(this):
         windowWidth = this.window.get_width() 
         windowHeigth = this.window.get_height()
-        for widget in this.widgets:
+        for widget in this.widgetList:
             if widget.x + widget.w//2 < windowWidth and widget.y + widget.h//2 < windowHeigth:
                 widget.draw()
         pygame.display.update()
